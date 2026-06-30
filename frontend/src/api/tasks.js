34868aad200446fc1127311a1390bb37021dbf5e
anjali432-eng <goodfriend1997312@gmail.com>
2026-06-30 23:22:38 +0530

    url

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,20 +1,19 @@
 import axios from 'axios'
-
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getTasks(completed = null) {
   const params = completed === null ? {} : { completed }
-  const response = await axios.get(`${API_URL}/`, { params })
+  const response = await axios.get(`${API_URL}/tasks/`, { params })
   return response.data
 }
 
 export async function createTask(task) {
-  const response = await axios.post(`${API_URL}/`, task)
+  const response = await axios.post(`${API_URL}/tasks/`, task)
   return response.data
 }
 
 export async function updateTask(id, task) {
-  const response = await axios.put(`${API_URL}/${id}`, task)
+  const response = await axios.put(`${API_URL}/tasks/${id}`, task)
   return response.data
 }
 
@@ -23,6 +22,6 @@ export async function deleteTask(id) {
 }
 
 export async function toggleTask(id) {
-  const response = await axios.patch(`${API_URL}/${id}/toggle`)
+  const response = await axios.patch(`${API_URL}/tasks/${id}/toggle`)
   return response.data
 }
